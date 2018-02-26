45980e723e422344156287a4bd546e63b68193a7
yukinorisuguimoto <suguimotomon@hotmail.com>
2018-02-26 16:58:25 -0300

    second commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const database       = require('./app/config/db');
+const app            = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(database.url, (err, database) => {
+    if (err) return console.log(err)
+    
+    const db = database.db("Taikai");
+
+    require('./app/routes')(app, db);
+
+    app.listen(port, () => {
+      console.log('We are live on ' + port);
+    });               
+})
+// app.listen(port, () => {
+//   console.log('We are live on ' + port);
+// });
