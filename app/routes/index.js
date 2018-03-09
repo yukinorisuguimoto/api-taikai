cd81862bdb133c18f9d67dfa6daab66de744fcb4
yukinorisuguimoto <suguimotomon@hotmail.com>
2018-03-09 14:15:30 -0300

    Add hook route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,9 @@
 const noteRoutes = require('./note_routes');
+const hookRoutes = require('./hook');
 const categoriaRoutes = require('./categoria_routes');
 module.exports = function(app, db) {
     categoriaRoutes(app, db);
+    hookRoutes(app, db);
     // noteRoutes(app, db);  
     // Other route groups could go here, in the future
 };
